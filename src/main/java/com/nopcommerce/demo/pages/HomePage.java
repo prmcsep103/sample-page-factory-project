1cf607daf89b984dee534afa8eb97a70937ba8ca
prmcsep103 <primecommunity103@gmail.com>
2023-12-09 17:40:28 +0000

    Today's class update

--- src/main/java/com/nopcommerce/demo/pages/HomePage.java
+++ src/main/java/com/nopcommerce/demo/pages/HomePage.java
@@ -1,15 +1,27 @@
 package com.nopcommerce.demo.pages;
 
 import com.nopcommerce.demo.utility.Utility;
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
 
 /**
  * Created by Jay Vaghani
  */
 public class HomePage extends Utility {
 
-    By loginLink = By.linkText("Log in");
-    By registerLink = By.linkText("Register");
+
+
+    @CacheLookup
+    @FindBy(linkText = "Log in")
+    WebElement loginLink;
+
+//    By loginLink = By.linkText("Log in");
+
+    @CacheLookup
+    @FindBy(linkText = "Register")
+    WebElement registerLink;
+//    By registerLink = By.linkText("Register");
 
     public void clickOnLoginLink(){
         clickOnElement(loginLink);
